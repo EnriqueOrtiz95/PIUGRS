b3af7328994fbc50f0c150e4e7ee35d2994c3bca
EnriqueOrtiz95 <enortizlo@gmail.com>
2023-01-11 11:16:47 -0700

    Fixed error handling in Players view

--- pages/players.js
+++ pages/players.js
@@ -7,14 +7,25 @@ const Players = ({ players }) => {
   const [isELO, setIsELO] = useState(false);
   const [isCountry, setIsCountry] = useState(false);
   const [isPlayerList, setIsPlayerList] = useState(false);
+
   useEffect(() => {
-    if (players) {
+    setLoading();
+  }, []);
+
+
+  const setLoading = () => {
+    if(players?.length) {
       setIsLoading(false);
+      return;
     }
-  }, [players]);
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 2500);
+  };
+
   return (
     <Layout title={"Players"} description={"List of PIU Players"}>
-      <div className="px-16 py-8 bg-gray-form4 my-[5rem]">
+      <div className="w-[95%] mx-auto px-16 py-8 bg-gray-form4 my-[5rem]">
         <div className="flex flex-col md:flex-row justify-evenly">
           <div className="p-4 md:py-2 mx-auto text-gray-BA hover:bg-gray-form">
             <button
@@ -38,12 +49,11 @@ const Players = ({ players }) => {
           </div>
         </div>
       </div>
-      <div className="px-16 py-8 bg-gray-form4 my-[5rem]">
-        {isLoading ? <h1 className="text-white text-2xl">Loading..</h1> : null}
+      <div className="w-[95%] mx-auto px-16 py-8 bg-gray-form4 my-[5rem]">
         <h2 className="heading text-red-fond">Players</h2>
 
-        {players.length && (
-          <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 text-white">
+        {players?.length && (
+          <div className="grid gridPlayers-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 text-white">
             {players
               .sort((a, b) => {
                 if (isELO) {
@@ -58,6 +68,13 @@ const Players = ({ players }) => {
               ))}
           </div>
         )}
+        {
+          <h1 className="text-white text-3xl text-center">
+            {
+              isLoading ? "Loading..." : !players?.length && "No players found"
+            }
+          </h1>
+        }
       </div>
     </Layout>
   );
@@ -66,11 +83,22 @@ const Players = ({ players }) => {
 export default Players;
 
 export async function getStaticProps() {
-  const respuesta = await fetch(`${process.env.API_URL}/pumper/list`);
-  const players = await respuesta.json();
-  return {
-    props: {
-      players,
-    },
-  };
+  try {
+    const respuesta = await fetch(`${process.env.API_URL}/pumper/list`);
+    if (!respuesta.ok) {
+      return { notFound: true };
+    }
+    const players = await respuesta.json();
+    return {
+      props: {
+        players,
+      },
+    };
+  } catch (error) {
+    return {
+      props: {
+        notFound: true,
+      },
+    };
+  }
 }
